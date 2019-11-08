fb6e7ecf31e9eab61cf7c14e2260e6862febdde5
Brandon Bazar <bazarsoundz@gmail.com>
2019-11-07 18:17:04 -0600

    Initial commit

---
+++ src/components/home.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="home">
+<div class="page-grid">
+<div class="page-overview blue-gradient">
+          <div>
+          <h2>Design System 42</h2>
+          <p>The ultimate answer to a consistent and scalable design language, user experience, and digital identity is... Design System 42.</p>
+          <p>We challenge ourselves every day to make consistent, useful products that help make our users lives better. We acknowledge that this is a continually moving goal that we must adapt to over time, and we accept this challenge as a way to strive to be better tomorrow than we are today.</p>
+          </div>
+    </div>
+<div class="page-content">
+
+    <h1>Getting Started</h1>
+
+<div class="four-column-layout">
+           <router-link  to="/foundations">
+              <div class="overview-section-cta">
+                  <img alt="Foundations Overview" src="../images/foundations-graphic.svg">
+                  <h4>Foundations</h4>
+                  <p class="overview-section-desc">The guidelines and usage of the basic building blocks of DS42.</p>
+              </div>
+          </router-link>
+         <router-link  to="/componentsoverview">
+          <div class="overview-section-cta">
+              <img alt="Components Overview" src="../images/components-graphic.svg">
+              <h4>Components</h4>
+              <p class="overview-section-desc">A collection of DS42 components including code and guidelines.</p>
+          </div>
+        </router-link>
+        <router-link  to="/resources">
+            <div class="overview-section-cta">
+                <img alt="Resources" src="../images/resources-graphic.svg">
+                <h4>Resources</h4>
+                <p class="overview-section-desc">Download fonts, icon packs, design kits, etc.</p>
+            </div>
+          </router-link>
+      </div>
+
+</div>
+</div>
+</div>
+</template>
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,81 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import home from '@/components/home'
+import resources from '@/components/resources'
+import foundations from '@/components/foundations'
+import colors from '@/components/colors'
+import typography from '@/components/typography'
+import componentsoverview from '@/components/componentsoverview'
+import accordions from '@/components/accordions'
+import buttons from '@/components/buttons'
+import tables from '@/components/tables'
+import datepickers from '@/components/datepickers'
+import formfields from '@/components/formfields'
+import selection from '@/components/selection'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: home
+    },
+    {
+      path: '/resources',
+      name: 'resources',
+      component: resources
+    },
+    {
+      path: '/foundations',
+      name: 'foundations',
+      component: foundations
+    },
+    {
+      path: '/colors',
+      name: 'colors',
+      component: colors
+    },
+    {
+      path: '/componentsoverview',
+      name: 'componentsoverview',
+      component: componentsoverview
+    },
+    {
+      path: '/accordions',
+      name: 'accordions',
+      component: accordions
+    },
+    {
+      path: '/buttons',
+      name: 'buttons',
+      component: buttons
+    },
+    {
+      path: '/datepickers',
+      name: 'datepickers',
+      component: datepickers
+    },
+    {
+      path: '/formfields',
+      name: 'formfields',
+      component: formfields
+    },
+    {
+      path: '/tables',
+      name: 'tables',
+      component: tables
+    },
+    {
+      path: '/selection',
+      name: 'selection',
+      component: selection
+    },
+    {
+      path: '/typography',
+      name: 'typography',
+      component: typography
+    }
+  ]
+})
